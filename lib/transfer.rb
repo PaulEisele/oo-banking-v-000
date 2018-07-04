9f41242bc491fb76316d060a5e1120962b6be94d
paulheisele@gmail.com <paulheisele@gmail.com>
2018-07-04 14:58:34 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,27 @@
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :status
+  attr_reader
+
+  @@all = []
+
+  def initialize()
+    @receiver =
+    @sender =
+    @status = "pending"
+    @transfer =
+  end
+
+  def valid?
+
+  end
+
+  def execute_transaction
+
+  end
+
+  def reverse_transfer
+
+  end
+
 end
