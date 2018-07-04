371e678908f4a6d342988b02eb7a28449bcc5144
paulheisele@gmail.com <paulheisele@gmail.com>
2018-07-04 14:55:29 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -6,9 +6,25 @@ class BankAccount
   @@all = []
 
 
-  def initialize 
+  def initialize
     @name = name
     @balance = 1000
     @status = "open"
+  end
+
+  def deposit
+
+  end
+
+  def display_balance
+
+  end
+
+  def valid?
+
+  end
+
+  def close_account
+
   end
 end
